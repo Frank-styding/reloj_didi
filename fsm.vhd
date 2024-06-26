ac62ba1c03c225d3a27dcc9a2e555e09cc48260f
RGsevenDB <juanpablorie@gmail.com>
2024-06-26 10:19:46 -0500

    JP_Junio26_10:11

--- fsm.vhd
+++ fsm.vhd
@@ -15,18 +15,23 @@ port(signal modo: in std_logic_vector(1 downto 0);
 	  signal reset_n: in std_logic;
 	  signal jugador_act: in std_logic;
 ----------------------------------------------------	  
---	  signal reset_1h_j0: out std_logic;
---	  signal reset_1h_j1: out std_logic;
-
---	  signal act_16_j0: out std_logic;
---	  signal act_16_j1: out std_logic;
---	  
---	  signal gt_16_j0: in std_Logic;
---	  signal gt_1h_j0: in std_Logic;
---	  
---	  signal gt_16_j1: in std_Logic;
---	  signal gt_1h_j1: in std_Logic;
+	  signal gt_16_j0: in std_Logic;
+	  signal gt_1h_j0: in std_Logic;
 	  
+	  signal gt_16_j1: in std_Logic;
+	  signal gt_1h_j1: in std_Logic;
+	
+
+	  signal reset_1h_j0: out std_logic;
+	  signal reset_1h_j1: out std_logic;
+
+	  signal act_16_j0: out std_logic;
+	  signal act_16_j1: out std_logic;
+	  
+	
+	  
+	  signal looser_sel: out std_logic;
+	  signal en_light: out std_logic;
 --------------------------------------------------	  
 	  signal ini_pausa_j0: out std_logic;
 	  signal borrar_j0: out std_logic;
@@ -34,13 +39,12 @@ port(signal modo: in std_logic_vector(1 downto 0);
 	  signal ini_pausa_j1: out std_logic;
 	  signal borrar_j1: out std_logic;
 	  signal en_j1: out std_logic;
-	  signal en_sel: out std_logic;
-	  signal leds: out std_logic_vector(9 downto 0));
+	  signal en_sel: out std_logic);
 	
 end fsm;
 
 architecture strc_1 of fsm is
-type state is (ST_IDLE,ST_WAIT_CONFIG,ST_PLAYERS_CONFIG,ST_PLAYER_0_P,ST_PLAYER_0,ST_PLAYER_1,ST_PLAYER_1_P, ST_PLAYER_0_L,ST_PLAYER_1_L,ST_PLAYER_0_M,ST_PLAYER_1_M,ST_PLAYER_0_L1,ST_PLAYER_1_L1);
+type state is (ST_IDLE,ST_WAIT_CONFIG,ST_PLAYERS_CONFIG,ST_PLAYER_0_P,ST_PLAYER_0,ST_PLAYER_1,ST_PLAYER_1_P, ST_PLAYER_0_L,ST_PLAYER_1_L,ST_PLAYER_0_M,ST_PLAYER_1_M,ST_PLAYER_1_M_1,ST_PLAYER_1_M_0,ST_PLAYER_0_M_1,ST_PLAYER_0_M_0);
 signal state_next,state_reg: state;
 begin
 
@@ -70,7 +74,11 @@ case state_reg is
 		borrar_j0<='1';
 		borrar_j1<='1';
 		en_sel<='0';
-		leds<="0000000000";
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j0<='0';
+		reset_1h_j1<='0';
+		
 		if(config='1') then
 			state_next<=ST_WAIT_CONFIG;
 		else
@@ -88,7 +96,11 @@ case state_reg is
 		borrar_j0<='1';
 		borrar_j1<='1';
 		en_sel<='1';
-		leds<="0000000000";
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j0<='0';
+		reset_1h_j1<='0';
+		
 		if(config='0') then
 			state_next<=ST_PLAYERS_CONFIG;
 		else
@@ -106,7 +118,10 @@ case state_reg is
 		borrar_j0<='0';
 		borrar_j1<='0';
 		en_sel<='0';
-		leds<="0000000000";
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j0<='0';
+		reset_1h_j1<='0';
 		
 		if(ini_pausa='1') then
 			if(jugador_act='0') then
@@ -131,7 +146,11 @@ case state_reg is
 		borrar_j0<='0';
 		borrar_j1<='0';
 		en_sel<='0';
-		leds<="0000000000";
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j0<='0';
+		reset_1h_j1<='0';
+		
 		if(ini_pausa='1') then
 			state_next<=ST_PLAYER_0;
 		else
@@ -148,7 +167,11 @@ case state_reg is
 		borrar_j0<='0';
 		borrar_j1<='0';
 		en_sel<='0';
-		leds<="0000000000";
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j0<='0';
+		reset_1h_j1<='0';
+		
 		if(ini_pausa='1') then
 			state_next<=ST_PLAYER_1;
 		else
@@ -164,24 +187,15 @@ case state_reg is
 		borrar_j0<='0';
 		borrar_j1<='0';
 		en_sel<='0';
-		leds<="0000000000";
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j0<='0';
+		reset_1h_j1<='0';
+		
 		if(ini_pausa='0') then
 			state_next<=ST_PLAYER_0_P;
 		else
-		--if(modo = "10") then
-			--state_next<=ST_PLAYER_0_W;
-		--else
---			if(min_value_j0='1') then 
---				state_next<=ST_PLAYER_0_L;
---			else
---				if(jugador_act='1') then
---					state_next<=ST_PLAYER_0_M;
---				else
---					state_next<=ST_PLAYER_0;
---				end if;
---			end if;
-		--end if;
-			if(min_value_j0='1') then 
+			if(min_value_j0='1') and (gt_16_j0='0') then 
 				state_next<=ST_PLAYER_0_L;
 			else
 				if(jugador_act='1') then
@@ -206,26 +220,16 @@ case state_reg is
 		borrar_j0<='0';
 		borrar_j1<='0';
 		en_sel<='0';
-		leds<="0000000000";
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j0<='0';
+		reset_1h_j1<='0';
 		
 		
 		if(ini_pausa='0') then
 			state_next<=ST_PLAYER_1_P;
 		else
-		--if(modo = "10") then
-			--state_next<=ST_PLAYER_1_W;
-		--else
---			if(min_value_j1='1') then 
---				state_next<=ST_PLAYER_1_L;
---			else
---				if(jugador_act='1') then
---					state_next<=ST_PLAYER_1_M;
---				else
---					state_next<=ST_PLAYER_1;
---				end if;
---			end if;
-		--end if;
-			if(min_value_j1='1') then 
+			if(min_value_j1='1') and (gt_16_j1='0') then 
 				state_next<=ST_PLAYER_1_L;
 			else
 				if(jugador_act='0') then
@@ -235,9 +239,20 @@ case state_reg is
 				end if;
 			end if;
 		end if;
-		
+--------------------------------------------------------------------		
 	when ST_PLAYER_0_M =>
-		state_next<=ST_PLAYER_1;
+--		state_next<=ST_PLAYER_1;
+
+		if(modo="10") and (mov_j0_gt40='1') then
+			if(gt_1h_j0='0') then
+				state_next<=ST_PLAYER_0_M_0;
+			else
+				state_next<=ST_PLAYER_0_M_1;
+			end if;
+		else
+			state_next<=ST_PLAYER_1;
+		end if;
+		
 		ini_pausa_j0<='0';
 		ini_pausa_j1<='1';
 		en_j0<='1';
@@ -245,16 +260,60 @@ case state_reg is
 		borrar_j0<='0';
 		borrar_j1<='0';
 		en_sel<='0';
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j0<='0';
+		reset_1h_j1<='0';
 		
 		
 --		mov_j0_gt40<='1';
 --		act_16_j0<=not(min_value_j0) and mov_j0_gt40;
 --		reset_1h_j0<=gt_1h_j0;
+
+
+
+	when st_plaYER_0_M_0 =>
+		state_next<=ST_PLAYER_1;
+		ini_pausa_j0<='0';
+		ini_pausa_j1<='0';
+		en_j0<='0';
+		en_j1<='0';
+		borrar_j0<='0';
+		borrar_j1<='0';
+		en_sel<='0';
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j0<='0';
+		reset_1h_j1<='0';
+
+	when ST_PLAYER_0_M_1 =>
+		state_next<=ST_PLAYER_1;
+		ini_pausa_j0<='0';
+		ini_pausa_j1<='0';
+		en_j0<='0';
+		en_j1<='0';
+		borrar_j0<='0';
+		borrar_j1<='0';
+		en_sel<='0';
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j0<='1';
+		reset_1h_j1<='0';
 		
-		leds<="0000000000";
-		
+-------------------------------------------------------------------		
 	when ST_PLAYER_1_M =>
-		state_next<=ST_PLAYER_0;
+--		state_next<=ST_PLAYER_0;
+
+	   if(modo="10") and (mov_j1_gt40='1') then
+			if(gt_1h_j1='0') then
+				state_next<=ST_PLAYER_1_M_0;
+			else
+				state_next<=ST_PLAYER_1_M_1;
+			end if;
+		else
+			state_next<=ST_PLAYER_0;
+		end if;
+		
 		ini_pausa_j0<='1';
 		ini_pausa_j1<='0';
 		en_j0<='0';
@@ -262,14 +321,46 @@ case state_reg is
 		borrar_j0<='0';
 		borrar_j1<='0';
 		en_sel<='0';
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j0<='0';
+		reset_1h_j1<='0';
 		
 		
 --		mov_j1_gt40<='1';
 --		act_16_j1<=not(min_value_j1) and mov_j1_gt40;
 --		reset_1h_j1<=gt_1h_j1;
-		
-		
-		leds<="0000000000";
+
+		when st_plaYER_1_M_0 =>
+		state_next<=ST_PLAYER_1;
+		ini_pausa_j0<='0';
+		ini_pausa_j1<='0';
+		en_j0<='0';
+		en_j1<='0';
+		borrar_j0<='0';
+		borrar_j1<='0';
+		en_sel<='0';
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j1<='0';
+		reset_1h_j0<='0';
+
+	when ST_PLAYER_1_M_1 =>
+		state_next<=ST_PLAYER_1;
+		ini_pausa_j0<='0';
+		ini_pausa_j1<='0';
+		en_j0<='0';
+		en_j1<='0';
+		borrar_j0<='0';
+		borrar_j1<='0';
+		en_sel<='0';
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j1<='1';
+		reset_1h_j0<='0';
+
+
+---------------------------------------------------------------		
 	
 	when ST_PLAYER_0_L =>
 		state_next<=ST_PLAYER_0_L;
@@ -280,18 +371,11 @@ case state_reg is
 		borrar_j0<='0';
 		borrar_j1<='0';
 		en_sel<='0';
-		leds<="1010100000";
+		looser_sel<='0';
+		en_light<='1';
+		reset_1h_j0<='0';
+		reset_1h_j1<='0';
 	
-	when ST_PLAYER_0_L1 =>
-		state_next<=ST_PLAYER_0_L;
-		ini_pausa_j0<='0';
-		ini_pausa_j1<='0';
-		en_j0<='0';
-		en_j1<='0';
-		borrar_j0<='0';
-		borrar_j1<='0';
-		en_sel<='0';
-		leds<="0101000000";
 		
 		
 	when ST_PLAYER_1_L =>
@@ -303,19 +387,10 @@ case state_reg is
 		borrar_j0<='0';
 		borrar_j1<='0';
 		en_sel<='0';
-		leds<="0000010101";
-	
-	when ST_PLAYER_1_L1 =>
-		state_next<=ST_PLAYER_1_L;
-		ini_pausa_j0<='0';
-		ini_pausa_j1<='0';
-		en_j0<='0';
-		en_j1<='0';
-		borrar_j0<='0';
-		borrar_j1<='0';
-		en_sel<='0';
-		leds<="0000001010";
-		
+		looser_sel<='1';
+	   en_light<='1';	
+		reset_1h_j0<='0';
+		reset_1h_j1<='0';
 	
 	
 	when others => state_next<=ST_IDLE;
@@ -325,7 +400,11 @@ case state_reg is
 		en_j1<='0';
 		borrar_j0<='1';
 		borrar_j1<='1';
-		leds<="0000000000";
+		en_sel<='0';
+		looser_sel<='0';
+		en_light<='0';
+		reset_1h_j0<='0';
+		reset_1h_j1<='0';
 end case;
 	
 
